d4d176597ad2e0a6f9aaf34ba39f99cfd5b09d25
iamvanillababy <sweetsweetcreamroll@gmail.com>
2024-06-08 12:59:06 -0700

    fixes bugs

--- src/WorkPage/NavBar.js
+++ src/WorkPage/NavBar.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
 const NavBar = () => {
-
+  const [walletAddress, setWalletAddress] = useState(sessionStorage.getItem("walletAddress") || null);
 
   const connectToMetaMask = async () => {
     try {
@@ -11,39 +11,77 @@ const NavBar = () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        sessionStorage.setItem("walletAddress",address);
+        sessionStorage.setItem("walletAddress", address);
+        setWalletAddress(address);
         console.log("Connected to MetaMask with address:", address);
-      
       } else {
         console.log("MetaMask extension not detected");
+        alert("MetaMask extension not detected");
       }
     } catch (error) {
       console.error("Error connecting to MetaMask:", error);
+      alert("Error connecting to MetaMask");
     }
   };
 
   useEffect(() => {
-    connectToMetaMask();
-  },[])
+    const checkConnection = async () => {
+      if (window.ethereum && walletAddress) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await provider.listAccounts();
+        if (accounts.length > 0) {
+          console.log("Wallet is already connected with address:", walletAddress);
+        } else {
+          sessionStorage.removeItem("walletAddress");
+          setWalletAddress(null);
+        }
+      }
+    };
+
+    checkConnection();
+
+    // Optional: Add event listeners for account and network changes
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", () => {
+        sessionStorage.removeItem("walletAddress");
+        setWalletAddress(null);
+      });
+
+      window.ethereum.on("disconnect", () => {
+        sessionStorage.removeItem("walletAddress");
+        setWalletAddress(null);
+      });
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener("accountsChanged", () => {
+          sessionStorage.removeItem("walletAddress");
+          setWalletAddress(null);
+        });
+
+        window.ethereum.removeListener("disconnect", () => {
+          sessionStorage.removeItem("walletAddress");
+          setWalletAddress(null);
+        });
+      }
+    };
+  }, [walletAddress]);
 
   return (
-    <div class=" px-1  ">
-      <header class="w-full font-myfont mt-5 text-gray-700 bg-[#f8fbff] rounded-md  shadow-md body-font">
-        <div class="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
-          <a class="flex items-center mb-4  text-xl font-bold   text-gray-900 title-font md:mb-0">
+    <div className="px-1">
+      <header className="w-full font-myfont mt-5 text-gray-700 bg-[#f8fbff] rounded-md shadow-md body-font">
+        <div className="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
+          <a className="flex items-center mb-4 text-xl font-bold text-gray-900 title-font md:mb-0">
             Sponsify
           </a>
-
-          
-          <div class="flex space-x-4 items-center justify-center h-full">
-
+          <div className="flex space-x-4 items-center justify-center h-full">
             <p className="text-[#7f98e9] text-lg cursor-pointer">Be a sponsor</p>
             <button
               onClick={connectToMetaMask}
-              className="px-4 py-2  bg-[#7f98e9]
-                    text-xs font-bold text-white uppercase transition-all duration-150  rounded shadow outline-none hover:shadow-md focus:outline-none ease"
+              className="px-4 py-2 bg-[#7f98e9] text-xs font-bold text-white uppercase transition-all duration-150 rounded shadow outline-none hover:shadow-md focus:outline-none ease"
             >
-              {sessionStorage.getItem("walletAddress")!==null?"Connected":"Connect Wallet"}
+              {walletAddress ? `Connected: ${walletAddress.slice(0, 6)}...${walletAddress.slice(-4)}` : "Connect Wallet"}
             </button>
           </div>
         </div>
--- src/WorkPage/Transaction.js
+++ src/WorkPage/Transaction.js
@@ -2,46 +2,41 @@ import React, { useMemo, useEffect, useState } from "react";
 import { ethers } from "ethers";
 import Abi from "../artifact/Sponsify.json";
 
-const Transaction = ({ ispending, setispending, onClose, loading, setLoading }) => {
-  const contractAddress = "0x7fBBdb1Db302C06750a30cA825Dca9c3B6e76135";
-  const RELAYER_PRIVATE_KEY ="baced1297f569a8e02962b7618c4ce6fc9bff2fd394849e0249175608a2597ae";
-  const _RELAYER_PRIVATE_KEY = process.env.REACT_APP_RELAYER_PRIVATE_KEY;
+const Transaction = ({ setispending, onClose, setLoading }) => {
+  const contractAddress = "0xc7A72d861A0F02Dde2dC6a872d3fAA28c3dbbb94";
+  const RELAYER_PRIVATE_KEY =
+    process.env.REACT_APP_RELAYER_PRIVATE_KEY ||
+    "cf47abdb48329f76a36de63ec047e78a298c26d490b247dcbdaf21f42ec73df9";
   const providerUrl = "https://pre-rpc.bittorrentchain.io/";
 
   const ethereum = useMemo(() => window.ethereum, []);
+
+  const [buttonState, setButtonState] = useState("Sign Transaction");
 
   useEffect(() => {
     if (!ethereum) {
       alert("Please initialize Ethereum");
     }
-  }, [ethereum]);
+  }, []);
 
   const [recipient, setRecipient] = useState("");
   const [amount, setAmount] = useState("");
   const [tokenAddress, setTokenAddress] = useState("");
 
-  const usdtTokenAddresses = [
-    { label: "USDT", value: "0xb1ea59521a88405d313d412f3f3efcf4a329f2dc" },
-    // Add more token addresses as needed
+  // const hashMessage = (message) => {
+  //   return ethers.utils.id(message);
+  // };
+
+  const TokenAbi = [
+    "function approve(address _spender, uint256 _value) external returns (bool success)",
+    "function allowance(address owner, address spender) external view returns (uint256)",
   ];
 
-  const hashMessage = (message) => {
-    return ethers.utils.id(message);
-  };
-
-  const relayTransaction = async (signedTransaction, transaction, signerAddress) => {
+  const relayTransaction = async (transaction) => {
     setLoading(true);
     try {
       const provider = new ethers.providers.JsonRpcProvider(providerUrl);
       const wallet = new ethers.Wallet(RELAYER_PRIVATE_KEY, provider);
-      
-      const messageSigner = await signedTransaction.then((value) =>
-        ethers.utils.recoverAddress(hashMessage(JSON.stringify(transaction)), value)
-      );
-
-      if (messageSigner !== signerAddress) {
-        throw new Error("Signer address mismatch");
-      }
 
       const txResponse = await wallet.sendTransaction(transaction);
       const receipt = txResponse.hash;
@@ -60,20 +55,58 @@ const Transaction = ({ ispending, setispending, onClose, loading, setLoading }) 
     }
   };
 
+  const getAllowance = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+    const wallet = new ethers.Wallet(RELAYER_PRIVATE_KEY, provider);
+    const tokenContract = new ethers.Contract(tokenAddress, TokenAbi, wallet);
+
+    const allowance = await tokenContract.allowance(
+      wallet.address,
+      contractAddress
+    );
+    return allowance;
+  };
+
+  const approveTokenByRelayer = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+    const wallet = new ethers.Wallet(RELAYER_PRIVATE_KEY, provider);
+    const tokenContract = new ethers.Contract(tokenAddress, TokenAbi, wallet);
+
+    const amountInWei = ethers.utils.parseUnits(amount, 18); // Adjust decimals if needed
+    const allowance = await getAllowance();
+
+    if (allowance.lt(amountInWei)) {
+      const tx = await tokenContract.approve(contractAddress, amountInWei);
+      await tx.wait();
+      console.log("Token approved by relayer:", tx);
+    } else {
+      console.log("Token already approved with sufficient allowance");
+    }
+  };
+
   const signAndRelayTransferUSDT = async () => {
     try {
+      setButtonState("Checking for approval..");
+      await approveTokenByRelayer();
+
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, Abi.abi, signer);
 
-      const data = contract.interface.encodeFunctionData("transferUSDT", [recipient, amount]);
+      const data = contract.interface.encodeFunctionData("transferUSDT", [
+        tokenAddress,
+        recipient,
+        amount,
+      ]);
+
+      console.log("data", data);
       const transaction = { to: contractAddress, data };
 
-      const signedTransaction = await signer.signMessage(JSON.stringify(transaction));
-      const signerAddress = await signer.getAddress();
-
-      await relayTransaction(signedTransaction, transaction, signerAddress);
       onClose();
+
+      setButtonState("Sign Transaction");
+
+      await relayTransaction(transaction);
     } catch (error) {
       console.error("Error signing and relaying transaction:", error);
     }
@@ -92,7 +125,9 @@ const Transaction = ({ ispending, setispending, onClose, loading, setLoading }) 
       <div className="max-w-xl text-[#131f38] font-myfont mx-auto bg-[#f8fbff] rounded-lg p-12 shadow-lg">
         <h2 className="text-2xl font-bold mb-6">Sign Transaction</h2>
         <div className="mb-6">
-          <label htmlFor="recipient" className="font-semibold mb-2">Recipient Address</label>
+          <label htmlFor="recipient" className="font-semibold mb-2">
+            Recipient Address
+          </label>
           <input
             type="text"
             id="recipient"
@@ -103,7 +138,9 @@ const Transaction = ({ ispending, setispending, onClose, loading, setLoading }) 
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="amount" className="font-semibold mb-4">Token Amount</label>
+          <label htmlFor="amount" className="font-semibold mb-4">
+            Token Amount
+          </label>
           <input
             type="text"
             id="amount"
@@ -113,27 +150,27 @@ const Transaction = ({ ispending, setispending, onClose, loading, setLoading }) 
             placeholder="Enter token amount"
           />
         </div>
+
         <div className="mb-6">
-          <label htmlFor="tokenAddress" className="font-semibold mb-4">USDT Token Address</label>
-          <select
+          <label htmlFor="tokenAddress" className="font-semibold mb-4">
+            Token Address
+          </label>
+          <input
+            type="text"
             id="tokenAddress"
             value={tokenAddress}
             onChange={(e) => setTokenAddress(e.target.value)}
             className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-400"
-            required
-          >
-            <option value="" disabled>Select USDT token address</option>
-            {usdtTokenAddresses.map((token) => (
-              <option key={token.value} value={token.value}>{token.label}</option>
-            ))}
-          </select>
+            placeholder="Enter token address"
+          />
         </div>
+
         <div className="space-x-4">
           <button
             onClick={signAndRelayTransferUSDT}
             className="mt-2 text-white bg-[#7f98e9] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            Sign transaction
+            {buttonState}
           </button>
           <button
             onClick={onClose}
